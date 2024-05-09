5409fff545ce58f92cbd15cfd60a09264384ce4c
AhsanRafiq8844 <rafiq.ahsan2134@gmail.com>
2024-05-09 06:23:44 -0700

    code added

--- lib/core/network/api_provider_impl.dart
+++ lib/core/network/api_provider_impl.dart
@@ -2,35 +2,102 @@ import 'package:dio/dio.dart';
 import 'package:ecommerce_user/core/network/api_provider.dart';
 import 'package:ecommerce_user/core/utilities/endpoints.dart';
 
-
-class APIProviderImpl implements APIProvider{
+class APIProviderImpl implements APIProvider {
   final Dio dio = Dio(
     BaseOptions(
-      baseUrl; baseUrlPath,
+      baseUrl: baseUrlPath,
       receiveDataWhenStatusError: true,
       connectTimeout: 5000,
     ),
-
   );
-  @override 
+  @override
   Future<Response> get(
-    {String? baseUrl,
-    required String endPoint,
-    data,
-    query,
-    String? token,
-    CancelToken? cancelToken,
-    int? timeout,
-    bool isMultipart = false}
-  )async {
-    if (timeOut != null){
+      {String? baseUrl,
+      required String endPoint,
+      data,
+      query,
+      String? token,
+      CancelToken? cancelToken,
+      int? timeOut,
+      bool isMultipart = false}) async {
+    if (timeOut != null) {
       dio.options.connectTimeout = timeOut;
+    }
 
-    }
     dio.options.headers = {
-      if(isMultipart)'Content-Type': 'multipart/form-data',
-      if(!isMultipart)' Content-Type': 'application/json',
-      
+      if (isMultipart) 'Content-Type': 'multipart/form-data',
+      if (!isMultipart) 'Content-Type': 'application/json',
+      if (!isMultipart) 'Accept': 'application/json',
+      if (token != null) 'Authorization': 'Bearer $token',
+      'lang': 'en',
+    };
+    return await dio.get(
+      endPoint,
+      queryParameters: query,
+      cancelToken: cancelToken,
+    );
+  }
+
+  @override
+  Future<Response> post(
+      {String? baseUrl,
+      required String endPoint,
+      data,
+      query,
+      String? token,
+      ProgressCallback? progressCallback,
+      CancelToken? cancelToken,
+      int? timeOut,
+      bool isMultipart = false}) async {
+    if (timeOut != null) {
+      dio.options.connectTimeout = timeOut;
     }
+
+    dio.options.headers = {
+      if (isMultipart) 'Content-Type': 'multipart/form-data',
+      if (!isMultipart) 'Content-Type': 'application/json',
+      if (!isMultipart) 'Accept': 'application/json',
+      if (token != null) 'Authorization': 'Bearer $token',
+      'lang': 'en',
+    };
+
+    return await dio.post(
+      endPoint,
+      data: data,
+      queryParameters: query,
+      onSendProgress: progressCallback,
+      cancelToken: cancelToken,
+    );
+  }
+
+  @override
+  Future<Response> put(
+      {String? baseUrl,
+      required String endPoint,
+      data,
+      query,
+      String? token,
+      ProgressCallback? progressCallback,
+      CancelToken? cancelToken,
+      int? timeOut,
+      bool isMultipart = false}) async {
+    if (timeOut != null) {
+      dio.options.connectTimeout = timeOut;
+    }
+
+    dio.options.headers = {
+      if (isMultipart) 'Content-Type': 'multipart/form-data',
+      if (!isMultipart) 'Content-Type': 'application/json',
+      if (!isMultipart) 'Accept': 'application/json',
+      if (token != null) 'Authorization': 'Bearer $token',
+      'lang': 'en',
+    };
+    return await dio.put(
+      endPoint,
+      data: data,
+      queryParameters: query,
+      onSendProgress: progressCallback,
+      cancelToken: cancelToken,
+    );
   }
 }
