3505ba126a08febc71bf5c47f186d406a647577c
AhsanRafiq8844 <rafiq.ahsan2134@gmail.com>
2024-05-16 01:46:26 -0700

    changes in code

--- lib/core/theme/bloc/theme_bloc.dart
+++ lib/core/theme/bloc/theme_bloc.dart
@@ -1,23 +1,22 @@
+import 'package:ecommerce_user/core/theme/theme_data.dart';
+import 'package:ecommerce_user/core/theme_service.dart';
+import 'package:ecommerce_user/core/utilities/enum.dart';
 import 'package:equateable/equateable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ecommerce_user/core/theme_service.dart';
-import 'package:ecommerce_user/core/utilities/enum.dart';
-import 'package:ecommerce_user/core/theme/theme_data.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  ThemeData? themeData = appThemeData[AppTheme.lghtTheme];
+  ThemeData? themeData = appThemeData[AppTheme.lightTheme];
   ThemeBloc()
-  :super(ThemeState(
-    themedata: appThemeData[
-      AppTheme.values[ThemeDataBaseService.getthemeSettings()]]!))
-      {
-        on<ThemeChanged>((event, emit)){
-          emit.call(ThemeState(themeData: appThemeData[event.theme]!));
-          themeData = appThemeData[event.theme];
-        });
-      }
+      : super(ThemeState(
+            themeData: appThemeData[
+                AppTheme.values[ThemeDatabaseService.getThemeSettings()]]!)) {
+    on<ThemeChanged>((event, emit) {
+      emit.call(ThemeState(themeData: appThemeData[event.theme]!));
+      themeData = appThemeData[event.theme];
+    });
+  }
 }
