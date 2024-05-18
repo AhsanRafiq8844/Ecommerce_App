55c509b394844a1e3bc616eff69ea1ae23026b18
AhsanRafiq8844 <rafiq.ahsan2134@gmail.com>
2024-05-18 05:27:49 -0700

    local data source

---
+++ lib/features/cart/data/datasource/local_datasource.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce_user/core/local/cache_manager.dart';
+import 'package:ecommerce_user/features/cart/data/models/cart_model.dart';
+import 'package:hive/hive.dart';
+
+class CartLocalDataSourceManager implements CacheManagerBase<CartProduct> {
+  @override
+  Future<void> init() async {
+    registerAdapter();
+    await Hive.openBox<CartProduct>("product-cahce");
+  }
+
+  @override
+  Future<void> addItem(CartProduct item) async {
+    await Hive.box("product-cahce").add(item);
+  }
+
+  @override
+  CartProduct? getItem(String key) {
+    return Hive.box("product-cahce").get(
+      key,
+    );
+  }
+
+  @override
+  Future<void> putItem(CartProduct item) async {
+    await Hive.box("product-cahce").put("product-cahce", item);
+  }
+
+  @override
+  Future<void> clearAll() async {
+    await Hive.box("product-cahce").clear();
+  }
+
+  @override
+  void registerAdapter() {
+    if (!Hive.isAdapterRegistered(4)) {
+      Hive.registerAdapter(CartProductAdapter());
+    }
+  }
+
+  @override
+  Future<void> clearAt(int id) async {
+    await Hive.box("product-cahce").deleteAt(id);
+  }
+}
